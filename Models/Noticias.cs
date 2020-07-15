766f5b18157c67e9141cd1d365ee318e2c163670
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-07-14 21:12:38 -0300

    Aula Models finalizada

---
+++ Models/Noticias.cs
@@ -0,0 +1,10 @@
+namespace E_PLAYERS_AspNetCore.Models
+{
+    public class Noticias
+    {
+        public int IdNoticia { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public string Imagem { get; set; }
+    }
+}
