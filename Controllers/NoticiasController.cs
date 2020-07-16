14db618fe1c2290fdddf0ea651d0f2a19cbf5e34
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-07-15 22:58:32 -0300

    Excluir e Upload de Imagens incrementados em Noticias

--- Controllers/NoticiasController.cs
+++ Controllers/NoticiasController.cs
@@ -7,11 +7,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using E_PLAYERS_AspNetCore.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace E_PLAYERS_AspNetCore.Controllers
 {
     public class NoticiasController : Controller
     {
+        /// <summary>
+        /// Aponta para a Index da View
+        /// </summary>
+        /// <returns>View</returns>
         Noticias noticiasModel = new Noticias();
         public IActionResult Index()
         {
@@ -23,20 +28,54 @@ namespace E_PLAYERS_AspNetCore.Controllers
         /// Publica as informações inseridas
         /// </summary>
         /// <param name="form">entrada de informações</param>
-        /// <returns></returns>
+        /// <returns>redireciona para mesma pagina</returns>
         public IActionResult Publicar(IFormCollection form)
         {
             Noticias noticias = new Noticias();
             noticias.IdNoticia = Int32.Parse( form["IdNoticia"]);
             noticias.Titulo   = form["Titulo"];
             noticias.Texto    = form["Texto"];
-            // caminho da imagem
-            noticias.Imagem   = form["Imagem"];
+            // Inicio Upload Imagem
+            // pegando arquivo do form no indice 0
+            var file    = form.Files[0];
+
+            // Mapeando a pasta
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Noticias");
+
+            // Condicional para criação de pasta (se não houver)
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                // Criando a url do arquivo
+                // Captura nome do arquivo = FileName
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Noticias", folder, file.FileName);
+                // Cadastra a imagem
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                // Passando nome do arquivo para o objeto
+                noticias.Imagem   = file.FileName;
+            }
+            else
+            {
+                noticias.Imagem   = "padrao.png";
+            }
+            // Fim Upload Imagem
 
             noticiasModel.Create(noticias);
-            
-            ViewBag.Noticias = noticiasModel.ReadAll();
             return LocalRedirect("~/Noticias");
+        }
+
+        [Route("Noticias/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            noticiasModel.Delete(id);
+            return LocalRedirect("~/Noticias");
+
         }
 
     }
